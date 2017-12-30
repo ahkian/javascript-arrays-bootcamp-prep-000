fc03298a8814e384b9dcb4e70e58e878161aaa11
Daniel Brazel <dbrazel4@gmail.com>
2017-12-30 20:57:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, newEl){
+  return [newEl, ...array]
+}
+
+function destructivelyAddElementsToBeginningOfArray(array, newEl){
+  array.unshift(newEl)
+  return array
+}
+
+function addElementToEndOfArray(array, newEl){
+  return [...array, newEl]
+}
+
+function destructivelyAddElementsToEndOfArray(array, newEl){
+  array.push(newEl)
+  return array
+}
