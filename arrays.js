707973dc913d551c4f38ad108fb4b170891c9c48
Daniel Brazel <dbrazel4@gmail.com>
2017-12-30 21:23:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 }
 
 function removeElementFromBeginningOfArray(arr){
-  arr.slice(1)
+  return arr.slice(1, 1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(arr){
